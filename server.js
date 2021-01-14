34434d67131a01af2ad8586f3eb87d6dd2549f33
marcotordera <marcotordera@gmail.com>
2021-01-14 13:29:51 -0800

    make it better

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === "production") {
   //set static folder
   app.use(express.static("client/build"));
-  server.use("*", express.static("client/build")); // Added this
-
+  app.use("*", express.static("client/build")); // Added this
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
